4c4542ebfa5d173937859621d4b6afc35e79aacc
bryan <24bryan.jonathan@gmail.com>
2025-05-12 05:21:41 +0800

    redesign back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,15 +1,21 @@
 'use client';
+
 import { useRouter } from 'next/navigation';
+import { ArrowLeft } from 'lucide-react';
 
 export default function BackButton() {
   const router = useRouter();
 
   return (
-    <button
-      onClick={() => router.back()}
-      className="mb-6 px-4 py-2 bg-gray-200 text-gray-700 rounded hover:bg-blue-600 hover:text-white transition"
-    >
-      ← Back
-    </button>
+    // <div className="px-4 sm:px-6 lg:px-8 mx-auto">
+    <div>
+      <button
+        onClick={() => router.back()}
+        className="inline-flex items-center gap-2 text-slate-600 hover:text-teal-700 transition-colors cursor-pointer"
+      >
+        <ArrowLeft className="h-4 w-4" />
+        <span>Back to Topics</span>
+      </button>
+    </div>
   );
 }
